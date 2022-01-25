5169f8fd04e19b5872548fc8d5ef3c495ace7d54
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2022-01-25 13:02:28 +0200

    Debug tests with Pdb

--- viikko3/login-robot/src/services/user_service.py
+++ viikko3/login-robot/src/services/user_service.py
@@ -1,4 +1,4 @@
-import re
+import re, sys, pdb
 from entities.user import User
 
 
@@ -15,6 +15,7 @@ class UserService:
         self._user_repository = user_repository
 
     def check_credentials(self, username, password):
+        pdb.Pdb(stdout=sys.__stdout__).set_trace()
         if not username or not password:
             raise UserInputError("Username and password are required")
 
