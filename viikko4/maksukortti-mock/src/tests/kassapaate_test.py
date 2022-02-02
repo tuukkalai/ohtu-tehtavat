1757bc17963f6a94a99a7283ba3072399784731f
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2022-02-02 19:38:45 +0200

    Fix and add kassapaate tests [4.2]

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,17 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    # Kassapäätteen metodin lataa kutsu lisää maksukortille ladattavan rahamäärän käyttäen kortin metodia lataa jos ladattava summa on positiivinen
+    def test_lisaa_kortille_rahaa_ladattaessa(self):
+        maksukortti_mock = Mock()
+        self.kassa.lataa(maksukortti_mock, 5)
+
+        maksukortti_mock.lataa.assert_called_with(5)
+
+    # Kassapäätteen metodin lataa kutsu ei tee maksukortille mitään jos ladattava summa on negatiivinen
+    def test_lisaa_kortille_ei_lisaa_negatiivista_summaa(self):
+        maksukortti_mock = Mock()
+        self.kassa.lataa(maksukortti_mock, -5)
+
+        maksukortti_mock.lataa.assert_not_called()
