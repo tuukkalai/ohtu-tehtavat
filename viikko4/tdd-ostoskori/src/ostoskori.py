381879b5866e83dd4202b490c0c04c33ac881d2e
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2022-02-03 18:35:03 +0200

    TDD: Adding one product to Ostoskori ostokset() -method returns correct list length [4.5]

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -5,21 +5,21 @@ from ostos import Ostos
 class Ostoskori:
     def __init__(self):
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
-        self.ostokset = []
+        self._ostokset = []
 
     def tavaroita_korissa(self):
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
-        return len(self.ostokset)
+        return len(self.ostokset())
 
     def hinta(self):
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
-        return reduce(lambda summa, tuote : summa + tuote.hinta(), self.ostokset, 0)
+        return reduce(lambda summa, tuote : summa + tuote.hinta(), self.ostokset(), 0)
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
-        self.ostokset.append(Ostos(lisattava))
+        self._ostokset.append(Ostos(lisattava))
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
@@ -30,6 +30,6 @@ class Ostoskori:
         # tyhjentää ostoskorin
 
     def ostokset(self):
-        pass
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
+        return self._ostokset
