29093c330abebeb8060d50f42de9843b3974e383
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2022-02-03 17:28:15 +0200

    TDD: Adding two different products updates amount of products in Ostoskori [4.5]

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -20,3 +20,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 3)
 
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        makkara = Tuote("Makkara", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(makkara)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
