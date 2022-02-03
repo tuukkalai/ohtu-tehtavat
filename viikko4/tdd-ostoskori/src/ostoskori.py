5d6e09e177f4fba53ce9b2cf305ebdd0b0015bb9
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2022-02-03 19:34:40 +0200

    Refactoring and cleaning [4.5]

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -11,14 +11,11 @@ class Ostoskori:
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
-        lkm = 0
-        for ostos in self.ostokset():
-            lkm += ostos.lukumaara()
-        return lkm
+        return reduce(lambda lukumaara, ostos: lukumaara + ostos.lukumaara(), self.ostokset(), 0)
 
     def hinta(self):
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
-        return reduce(lambda summa, tuote : summa + tuote.hinta(), self.ostokset(), 0)
+        return reduce(lambda summa, tuote: summa + tuote.hinta(), self.ostokset(), 0)
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -5,70 +5,59 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
+        self.maito = Tuote("Maito", 3)
+        self.makkara = Tuote("Makkara", 5)
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
 
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_tavara(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 1)
 
     def test_yhden_tuotteen_lisaaminen_paivittaa_ostoskorin_hintaa(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.hinta(), 3)
 
     def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
-        maito = Tuote("Maito", 3)
-        makkara = Tuote("Makkara", 5)
-        self.kori.lisaa_tuote(maito)
-        self.kori.lisaa_tuote(makkara)
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.makkara)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
 
     def test_kahden_eri_tuotteen_lisaaminen_paivittaa_ostoskorin_hintaa(self):
-        maito = Tuote("Maito", 3)
-        makkara = Tuote("Makkara", 5)
-        self.kori.lisaa_tuote(maito)
-        self.kori.lisaa_tuote(makkara)
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.makkara)
         self.assertEqual(self.kori.hinta(), 8)
 
     def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
 
     def test_kahden_saman_tuotteen_lisaaminen_paivittaa_ostoskorin_hintaa(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.hinta(), 6)
 
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(len(self.kori.ostokset()), 1)
 
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio_jolla_oikea_tuotteen_nimi_ja_maara(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
 
         ostos = self.kori.ostokset()[0]
-        self.assertEqual(ostos.tuotteen_nimi(), maito.nimi())
+        self.assertEqual(ostos.tuotteen_nimi(), self.maito.nimi())
         self.assertEqual(ostos.lukumaara(), 1)
 
     def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_kaksi_ostosoliota(self):
-        maito = Tuote("Maito", 3)
-        makkara = Tuote("Makkara", 5)
-        self.kori.lisaa_tuote(maito)
-        self.kori.lisaa_tuote(makkara)
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.makkara)
         self.assertEqual(len(self.kori.ostokset()), 2)
 
     def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(len(self.kori.ostokset()), 1)
     
