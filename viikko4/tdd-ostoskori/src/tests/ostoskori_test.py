763654b9c125307967a8a2fc2f6d0c4345f10291
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2022-02-03 18:22:21 +0200

    TDD: Adding two same products updates the total price of Ostoskori [4.5]

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -39,3 +39,9 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_saman_tuotteen_lisaaminen_paivittaa_ostoskorin_hintaa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.hinta(), 6)
